f8cee1816dd74df81946985852425f60ddef6d0e
İBRAHİM ELZAMİL <ibooalzam@gmail.com>
2021-03-19 11:10:05 +0300

    2

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Start start = new Start();
+            //start.CarTest();
+            //start.BrandTest();
+            start.ColorTest();
+        }
+    }
+}
